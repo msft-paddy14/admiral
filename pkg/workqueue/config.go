c2a2ae0857601215035541ad766db5af645b9ef9
Paddy <padgupta@microsoft.com>
2026-02-18 19:43:19 +0000

    CHANGE WORKERS

--- pkg/workqueue/config.go
+++ pkg/workqueue/config.go
@@ -51,7 +51,7 @@ func DefaultConfig() Config {
 		OverallRateLimiterMaxDelay:   5 * time.Minute,
 		BucketRateLimiterItemsPerSec: 1000,
 		BucketRateLimiterMaxBurst:    2000,
-		NumWorkers: 30,
+		NumWorkers: 10,
 	}
 }
 
